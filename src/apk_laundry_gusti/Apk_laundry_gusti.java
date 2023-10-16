eed3186165b95f27f13e5611001d50b2b366da43
GustiAditya3003 <adit94545@gmail.com>
2023-10-16 12:05:04 +0700

    tugas laundry pbo

---
+++ src/apk_laundry_gusti/Apk_laundry_gusti.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package apk_laundry_gusti;
+
+/**
+ *
+ * @author PC-32
+ */
+public class Apk_laundry_gusti {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
